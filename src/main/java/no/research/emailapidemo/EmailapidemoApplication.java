b4921fabd1f55335fa2768851e7abf2f84657345
kdm-kev-NTNU <kevindm@stud.ntnu.no>
2025-05-01 13:57:27 +0200

    first commit

---
+++ src/main/java/no/research/emailapidemo/EmailapidemoApplication.java
@@ -0,0 +1,13 @@
+package no.research.emailapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailapidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailapidemoApplication.class, args);
+	}
+
+}
